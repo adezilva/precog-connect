c67c87678e3e1589e5fb9324d9db521200c29cac
christian <43155813+cyber-shepherd@users.noreply.github.com>
2019-12-07 12:48:55 -0700

    Create umbrella.py

---
+++ umbrella.py
@@ -0,0 +1,3 @@
+#this script should create all neccessary functions to interact with Umbrella's API(s)
+#we wan't to query and handle the return of Ubrella's API for looking up IoCs; as well as the ability to report observations if available
+#this may be a copy and paste of Cisco documntation 
